268849857a42c7652d7e7b13fff080063b0e0c26
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-03-21 11:42:26 +0200

    fix file input accepted formats

--- projects/ng-getform/src/lib/components/file-input/drag-and-drop.directive.ts
+++ projects/ng-getform/src/lib/components/file-input/drag-and-drop.directive.ts
@@ -6,10 +6,9 @@ import { Directive, HostListener, HostBinding, Output, EventEmitter } from '@ang
 
 export class FileDragNDropDirective {
     @Output() private filesChangeEmiter: EventEmitter<File[]> = new EventEmitter();
-    @HostBinding('style.background') private background = '#eee';
-    @HostBinding('style.border') private borderStyle = '2px dashed';
-    @HostBinding('style.border-color') private borderColor = '#696D7D';
-    @HostBinding('style.border-radius') private borderRadius = '5px';
+    @HostBinding('style.background-color') private background = 'rgba(241, 242, 244, 0.5)';
+    @HostBinding('style.border') private borderStyle = '1px dashed';
+    @HostBinding('style.border-color') private borderColor = '#9B9CA0';
 
     constructor() { }
 
@@ -17,24 +16,24 @@ export class FileDragNDropDirective {
         evt.preventDefault();
         evt.stopPropagation();
         this.background = 'lightgray';
-        this.borderColor = 'cadetblue';
-        this.borderStyle = '3px solid';
+        this.borderColor = '#2F55B7';
+        this.borderStyle = '2px solid';
     }
 
     @HostListener('dragleave', ['$event']) public onDragLeave(evt: Event) {
         evt.preventDefault();
         evt.stopPropagation();
-        this.background = '#eee';
-        this.borderColor = '#696D7D';
+        this.background = 'rgba(241, 242, 244, 0.5)';
+        this.borderColor = '#9B9CA0';
         this.borderStyle = '2px dashed';
     }
 
     @HostListener('drop', ['$event']) public onDrop(evt: any) {
         evt.preventDefault();
         evt.stopPropagation();
-        this.background = '#eee';
-        this.borderColor = '#696D7D';
-        this.borderStyle = '2px dashed';
+        this.background = 'rgba(241, 242, 244, 0.5)';
+        this.borderColor = '#9B9CA0';
+        this.borderStyle = '1px dashed';
         const files = evt.dataTransfer.files;
         const valid_files: Array<File> = files;
         this.filesChangeEmiter.emit(valid_files);
--- projects/ng-getform/src/lib/components/file-input/file-input.component.ts
+++ projects/ng-getform/src/lib/components/file-input/file-input.component.ts
@@ -1,5 +1,5 @@
+import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -8,30 +8,30 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./file-input.component.scss']
 })
 export class FileInputComponent {
+  @Input() isMultipleFiles: boolean = false;
   supportedFormats: string[] = ['JPEG', 'PNG', 'GIF', 'PDF', 'Word'];
   importFile: FormControl = new FormControl('')
   fileName: string = '';
-
 
   constructor(private http: HttpClient) {
 
   }
   onFileChange(e: any) {
-    const file = e.files[0];
-    console.log(e.files[0])
-    if (file) {
-      this.fileName = file.name;
-      const reader = new FileReader();
-      // addListeners(reader, file);
-      // reader.readAsDataURL(file);
-      console.log(reader)
-      // const formData = new FormData();
-      // formData.append("file", file);
-      // console.log(formData)
+    // const file = e.files[0];
+    console.log(e);
+    // if (file) {
+    //   this.fileName = file.name;
+    //   const reader = new FileReader();
+    // addListeners(reader, file);
+    // reader.readAsDataURL(file);
+    // console.log(reader)
+    // const formData = new FormData();
+    // formData.append("file", file);
+    // console.log(formData)
 
-      // const upload$ = this.http.post('/api/uploads', formData);
-      // upload$.subscribe();
-    }
+    // const upload$ = this.http.post('/api/uploads', formData);
+    // upload$.subscribe();
+    // }
   }
 
   labelClickHandler(e: Event) {
